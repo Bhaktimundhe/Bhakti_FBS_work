e6f45e63e6277a959ee4193ccfb09604f8d54f15
Bhaktimundhe <bhaktimundhe0712@gmail.com>
2025-07-24 23:32:37 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignment 1/program 6.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int a=5;
+	int sq=a*a;
+	int cu=a*a*a;
+	printf("square and cube of %d is %d,%d",a,sq,cu);
+	
+}
---
+++ C Programming/Assignments/Assignment 1/program 8.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int h=18;
+	int b=15;
+	int area=b*h;
+	int perimeter=2*(b+h);
+	printf("perimeter of rectangle is %d",perimeter);
+	printf("area of rectangle is %d",area);
+	
+}
